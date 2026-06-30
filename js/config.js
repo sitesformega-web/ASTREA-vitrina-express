4390abc5a5340cb92c08aa9f3961fac18caa46c6
sitesformega-web <sitesformega@gmail.com>
2026-06-30 17:11:55 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -58,6 +58,6 @@ const BUSINESS = {
   },
 
   api: {
-    endpoint: "https://script.google.com/macros/s/AKfycbzvkMSG84918hb3wsJEkrkmIH5ev1ZbqZqoHdVzNcBCMILaF52ypMdzmbtYtheBN8ga/exec"
+    endpoint: "https://script.google.com/macros/s/AKfycby_xfLI0rKIZrsPB5LlPqUUKdHvFfRN_cRqpKqI7bNeQZ-BMGMwOlocf-0wH2Glf6g2Cw/exec"
   }
 };
